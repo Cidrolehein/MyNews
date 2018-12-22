ba0138c4084d670ecf9844679de8bb6cad07a5e2
Julien Gacon <gaconjulien@gmail.com>
2018-12-22 16:29:11 +0100

    Add Toolbar and configure colors

--- app/src/main/java/com/gacon/julien/mynews/MainActivity.java
+++ app/src/main/java/com/gacon/julien/mynews/MainActivity.java
@@ -2,6 +2,9 @@ package com.gacon.julien.mynews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1 - Configure Toolbar
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        //2 - Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 3 - Handle actions on menu items
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_params:
+                Toast.makeText(this, "Il n'y a rien à paramétrer", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this,"Recherche indisponible", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return  super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void configureToolbar(){
+        //Get the toolbar view inside the activity layout
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        // Set the Toolbar
+        setSupportActionBar(toolbar);
     }
 }
