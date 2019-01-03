c6e49f51cd91e9e9f541611c5af633c0aa9d73fa
Julien Gacon <gaconjulien@gmail.com>
2019-01-03 17:17:16 +0100

    Add SwipeRefreshLayout

--- app/src/main/java/com/gacon/julien/mynews/Controllers/Fragments/MostPopularFragment.java
+++ app/src/main/java/com/gacon/julien/mynews/Controllers/Fragments/MostPopularFragment.java
@@ -2,11 +2,14 @@ package com.gacon.julien.mynews.Controllers.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
 import com.gacon.julien.mynews.Controllers.Utils.GithubStreams;
 import com.gacon.julien.mynews.Models.SearchArticleNewYorker;
 import com.gacon.julien.mynews.R;
@@ -25,8 +28,9 @@ public class MostPopularFragment extends Fragment {
     }
 
     // FOR DESIGN
-    @BindView(R.id.fragment_main_recycler_view)
-    RecyclerView recyclerView; // 1 - Declare RecyclerView
+    @BindView(R.id.fragment_main_recycler_view) RecyclerView recyclerView;
+    @BindView(R.id.fragment_main_swipe_container)
+    SwipeRefreshLayout swipeRefreshLayout;
 
     //FOR DATA
     private Disposable disposable;
@@ -40,7 +44,8 @@ public class MostPopularFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_most_popular, container, false);
         ButterKnife.bind(this, view);
-        this.configureRecyclerView(); // - 4 Call during UI creation
+        this.configureRecyclerView();
+        this.configureSwipeRefreshLayout(); // - 4 Call during UI creation
         this.executeHttpRequestWithRetrofit(); // 5 - Execute stream after UI creation
         return view;
     }
@@ -55,16 +60,24 @@ public class MostPopularFragment extends Fragment {
     // CONFIGURATION
     // -----------------
 
-    // 3 - Configure RecyclerView, Adapter, LayoutManager & glue it together
     private void configureRecyclerView(){
-        // 3.1 - Reset list
         this.githubUsers = new ArrayList<>();
-        // 3.2 - Create adapter passing the list of users
-        this.adapter = new GithubUserAdapter(this.githubUsers);
-        // 3.3 - Attach the adapter to the recyclerview to populate items
+        // Create adapter passing in the sample user data
+        this.adapter = new GithubUserAdapter(this.githubUsers, Glide.with(this));
+        // Attach the adapter to the recyclerview to populate items
         this.recyclerView.setAdapter(this.adapter);
-        // 3.4 - Set layout manager to position the items
+        // Set layout manager to position the items
         this.recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    // 3 - Configure RecyclerView, Adapter, LayoutManager & glue it together
+    private void configureSwipeRefreshLayout(){
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                executeHttpRequestWithRetrofit();
+            }
+        });
     }
 
     // -------------------
@@ -96,6 +109,9 @@ public class MostPopularFragment extends Fragment {
     // -------------------
 
     private void updateUI(List<SearchArticleNewYorker> users){
+        // 3 - Stop refreshing and clear actual list of users
+        swipeRefreshLayout.setRefreshing(false);
+        githubUsers.clear();
         githubUsers.addAll(users);
         adapter.notifyDataSetChanged();
     }
--- app/src/main/java/com/gacon/julien/mynews/Views/GithubUserAdapter.java
+++ app/src/main/java/com/gacon/julien/mynews/Views/GithubUserAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.RequestManager;
 import com.gacon.julien.mynews.Models.SearchArticleNewYorker;
 import com.gacon.julien.mynews.R;
 
@@ -13,12 +14,17 @@ import java.util.List;
 
 public class GithubUserAdapter extends RecyclerView.Adapter<GithubUserViewHolder> {
 
+    // 1 - Declaring a Glide object
+    private RequestManager glide;
+
     // FOR DATA
     private List<SearchArticleNewYorker> githubUsers;
 
     // CONSTRUCTOR
-    public GithubUserAdapter(List<SearchArticleNewYorker> githubUsers) {
+    // 2 - Updating our constructor adding a Glide Object
+    public GithubUserAdapter(List<SearchArticleNewYorker> githubUsers, RequestManager glide) {
         this.githubUsers = githubUsers;
+        this.glide = glide;
     }
 
     @Override
@@ -34,7 +40,8 @@ public class GithubUserAdapter extends RecyclerView.Adapter<GithubUserViewHolder
     // UPDATE VIEW HOLDER WITH A GITHUBUSER
     @Override
     public void onBindViewHolder(GithubUserViewHolder viewHolder, int position) {
-        viewHolder.updateWithGithubUser(this.githubUsers.get(position));
+        // - 3 Passing the Glide object to each ViewHolder
+        viewHolder.updateWithGithubUser(this.githubUsers.get(position), this.glide);
     }
 
     // RETURN THE TOTAL COUNT OF ITEMS IN THE LIST
