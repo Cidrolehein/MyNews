a614203521c869f44d6bd76913547cdd3e1140ff
Julien Gacon <gaconjulien@gmail.com>
2019-01-09 23:57:29 +0100

    NyTimes API start connexion

--- app/src/main/java/com/gacon/julien/mynews/Controllers/Fragments/MostPopularFragment.java
+++ app/src/main/java/com/gacon/julien/mynews/Controllers/Fragments/MostPopularFragment.java
@@ -1,25 +1,14 @@
 package com.gacon.julien.mynews.Controllers.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.bumptech.glide.Glide;
-import com.gacon.julien.mynews.Controllers.Utils.GithubStreams;
-import com.gacon.julien.mynews.Models.SearchArticleNewYorker;
 import com.gacon.julien.mynews.R;
-import com.gacon.julien.mynews.Views.GithubUserAdapter;
-import java.util.ArrayList;
-import java.util.List;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.observers.DisposableObserver;
+
 
 public class MostPopularFragment extends Fragment {
 
@@ -27,93 +16,9 @@ public class MostPopularFragment extends Fragment {
         return (new MostPopularFragment());
     }
 
-    // FOR DESIGN
-    @BindView(R.id.fragment_main_recycler_view) RecyclerView recyclerView;
-    @BindView(R.id.fragment_main_swipe_container)
-    SwipeRefreshLayout swipeRefreshLayout;
-
-    //FOR DATA
-    private Disposable disposable;
-    // 2 - Declare list of users (GithubUser) & Adapter
-    private List<SearchArticleNewYorker> githubUsers;
-    private GithubUserAdapter adapter;
-
-    public MostPopularFragment() { }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_most_popular, container, false);
-        ButterKnife.bind(this, view);
-        this.configureRecyclerView();
-        this.configureSwipeRefreshLayout(); // - 4 Call during UI creation
-        this.executeHttpRequestWithRetrofit(); // 5 - Execute stream after UI creation
-        return view;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        this.disposeWhenDestroy();
-    }
-
-    // -----------------
-    // CONFIGURATION
-    // -----------------
-
-    private void configureRecyclerView(){
-        this.githubUsers = new ArrayList<>();
-        // Create adapter passing in the sample user data
-        this.adapter = new GithubUserAdapter(this.githubUsers, Glide.with(this));
-        // Attach the adapter to the recyclerview to populate items
-        this.recyclerView.setAdapter(this.adapter);
-        // Set layout manager to position the items
-        this.recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-    }
-
-    // 3 - Configure RecyclerView, Adapter, LayoutManager & glue it together
-    private void configureSwipeRefreshLayout(){
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                executeHttpRequestWithRetrofit();
-            }
-        });
-    }
-
-    // -------------------
-    // HTTP (RxJAVA)
-    // -------------------
-
-    private void executeHttpRequestWithRetrofit(){
-        this.disposable = GithubStreams.streamFetchUserFollowing("JakeWharton").subscribeWith(new DisposableObserver<List<SearchArticleNewYorker>>() {
-            @Override
-            public void onNext(List<SearchArticleNewYorker> users) {
-                // 6 - Update RecyclerView after getting results from Github API
-                updateUI(users);
-            }
-
-            @Override
-            public void onError(Throwable e) { }
-
-            @Override
-            public void onComplete() { }
-        });
-    }
-
-    private void disposeWhenDestroy(){
-        if (this.disposable != null && !this.disposable.isDisposed()) this.disposable.dispose();
-    }
-
-    // -------------------
-    // UPDATE UI
-    // -------------------
-
-    private void updateUI(List<SearchArticleNewYorker> users){
-        // 3 - Stop refreshing and clear actual list of users
-        swipeRefreshLayout.setRefreshing(false);
-        githubUsers.clear();
-        githubUsers.addAll(users);
-        adapter.notifyDataSetChanged();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_most_popular, container, false);
     }
 
 }
--- app/src/main/java/com/gacon/julien/mynews/Controllers/Fragments/TopStoriesFragment.java
+++ app/src/main/java/com/gacon/julien/mynews/Controllers/Fragments/TopStoriesFragment.java
@@ -1,13 +1,25 @@
 package com.gacon.julien.mynews.Controllers.Fragments;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.gacon.julien.mynews.Controllers.Adapters.NyTimesAdapter;
+import com.gacon.julien.mynews.Controllers.Utils.NyTimesTopStoriesStreams;
+import com.gacon.julien.mynews.Models.MainNewYorkTimesTopStories;
 import com.gacon.julien.mynews.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 
 public class TopStoriesFragment extends Fragment {
 
@@ -15,9 +27,79 @@ public class TopStoriesFragment extends Fragment {
         return (new TopStoriesFragment());
     }
 
+    // FOR DESIGN
+    @BindView(R.id.fragment_main_recycler_view)
+    RecyclerView recyclerView; // 1 - Declare RecyclerView
+
+    //FOR DATA
+    private Disposable disposable;
+    // 2 - Declare list of users (GithubUser) & Adapter
+    private List<MainNewYorkTimesTopStories> mNyTopStories;
+    private NyTimesAdapter adapter;
+
+    public TopStoriesFragment() { }
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_top_stories, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_top_stories, container, false);
+        ButterKnife.bind(this, view);
+        this.configureRecyclerView(); // - 4 Call during UI creation
+        this.executeHttpRequestWithRetrofit(); // 5 - Execute stream after UI creation
+        return view;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
+
+    // -----------------
+    // CONFIGURATION
+    // -----------------
+
+    // 3 - Configure RecyclerView, Adapter, LayoutManager & glue it together
+    private void configureRecyclerView(){
+        // 3.1 - Reset list
+        this.mNyTopStories = new ArrayList<>();
+        // 3.2 - Create adapter passing the list of users
+        this.adapter = new NyTimesAdapter(this.mNyTopStories);
+        // 3.3 - Attach the adapter to the recyclerview to populate items
+        this.recyclerView.setAdapter(this.adapter);
+        // 3.4 - Set layout manager to position the items
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    // -------------------
+    // HTTP (RxJAVA)
+    // -------------------
+
+    private void executeHttpRequestWithRetrofit(){
+        this.disposable = NyTimesTopStoriesStreams.streamFetchTopStories("home").subscribeWith(new DisposableObserver<List<MainNewYorkTimesTopStories>>() {
+            @Override
+            public void onNext(List<MainNewYorkTimesTopStories> topStories) {
+                // 6 - Update RecyclerView after getting results from Github API
+                updateUI(topStories);
+            }
+
+            @Override
+            public void onError(Throwable e) { }
+
+            @Override
+            public void onComplete() { }
+        });
+    }
+
+    private void disposeWhenDestroy(){
+        if (this.disposable != null && !this.disposable.isDisposed()) this.disposable.dispose();
+    }
+
+    // -------------------
+    // UPDATE UI
+    // -------------------
+
+    private void updateUI(List<MainNewYorkTimesTopStories> topstories){
+        mNyTopStories.addAll(topstories);
+        adapter.notifyDataSetChanged();
+    }
 }
