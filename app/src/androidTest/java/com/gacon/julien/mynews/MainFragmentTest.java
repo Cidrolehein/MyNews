5550cc018a6b4a25413598b88a15f9be8856d91a
Julien Gacon <gaconjulien@gmail.com>
2019-01-18 16:07:05 +0100

    First functional Test for List<Result>

---
+++ app/src/androidTest/java/com/gacon/julien/mynews/MainFragmentTest.java
@@ -0,0 +1,41 @@
+package com.gacon.julien.mynews;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.gacon.julien.mynews.Controllers.Utils.NyTimesTopStoriesStreams;
+import com.gacon.julien.mynews.Models.MainNewYorkTimesTopStories;
+import com.gacon.julien.mynews.Models.Result;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class MainFragmentTest {
+
+    @Test
+    public void downloadNyTopStories() throws Exception {
+        // Get the stream
+        Observable<MainNewYorkTimesTopStories> observableNyTopStories =
+                NyTimesTopStoriesStreams.streamFetchTopStories("home");
+        // Create a new TestObserver
+        TestObserver<MainNewYorkTimesTopStories> testObserver = new TestObserver<>();
+        // Launch observable
+        observableNyTopStories.subscribeWith(testObserver)
+                .assertNoErrors()// Check if no errors
+                .assertNoTimeout() // Check if no Timeout
+                .awaitTerminalEvent(); // Await the stream terminated before continue
+
+        // Get list of articles results
+        MainNewYorkTimesTopStories articleResultsDownload = testObserver.values().get(0);
+
+        assertThat("Il y a un article qui s'est téléchargé", articleResultsDownload.getResults() != null);
+
+    }
+}
