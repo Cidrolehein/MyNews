69fab54aefbd425920e68295333dd45612bfa29f
Julien Gacon <gaconjulien@gmail.com>
2019-01-29 03:10:56 +0100

    Clean Models and Code

--- app/src/main/java/com/gacon/julien/mynews/Models/TopStories/Result.java
+++ app/src/main/java/com/gacon/julien/mynews/Models/TopStories/Result.java
@@ -22,12 +22,6 @@ public class Result {
     @SerializedName("url")
     @Expose
     private String url;
-    @SerializedName("byline")
-    @Expose
-    private String byline;
-    @SerializedName("item_type")
-    @Expose
-    private String itemType;
     @SerializedName("updated_date")
     @Expose
     private String updatedDate;
@@ -37,24 +31,6 @@ public class Result {
     @SerializedName("published_date")
     @Expose
     private String publishedDate;
-    @SerializedName("material_type_facet")
-    @Expose
-    private String materialTypeFacet;
-    @SerializedName("kicker")
-    @Expose
-    private String kicker;
-    @SerializedName("des_facet")
-    @Expose
-    private List<String> desFacet = null;
-    @SerializedName("org_facet")
-    @Expose
-    private List<String> orgFacet = null;
-    @SerializedName("per_facet")
-    @Expose
-    private List<String> perFacet = null;
-    @SerializedName("geo_facet")
-    @Expose
-    private List<String> geoFacet = null;
     @SerializedName("multimedia")
     @Expose
     private List<Multimedium> multimedia = null;
@@ -102,22 +78,6 @@ public class Result {
         this.url = url;
     }
 
-    public String getByline() {
-        return byline;
-    }
-
-    public void setByline(String byline) {
-        this.byline = byline;
-    }
-
-    public String getItemType() {
-        return itemType;
-    }
-
-    public void setItemType(String itemType) {
-        this.itemType = itemType;
-    }
-
     public String getUpdatedDate() {
         return updatedDate;
     }
@@ -140,54 +100,6 @@ public class Result {
 
     public void setPublishedDate(String publishedDate) {
         this.publishedDate = publishedDate;
-    }
-
-    public String getMaterialTypeFacet() {
-        return materialTypeFacet;
-    }
-
-    public void setMaterialTypeFacet(String materialTypeFacet) {
-        this.materialTypeFacet = materialTypeFacet;
-    }
-
-    public String getKicker() {
-        return kicker;
-    }
-
-    public void setKicker(String kicker) {
-        this.kicker = kicker;
-    }
-
-    public List<String> getDesFacet() {
-        return desFacet;
-    }
-
-    public void setDesFacet(List<String> desFacet) {
-        this.desFacet = desFacet;
-    }
-
-    public List<String> getOrgFacet() {
-        return orgFacet;
-    }
-
-    public void setOrgFacet(List<String> orgFacet) {
-        this.orgFacet = orgFacet;
-    }
-
-    public List<String> getPerFacet() {
-        return perFacet;
-    }
-
-    public void setPerFacet(List<String> perFacet) {
-        this.perFacet = perFacet;
-    }
-
-    public List<String> getGeoFacet() {
-        return geoFacet;
-    }
-
-    public void setGeoFacet(List<String> geoFacet) {
-        this.geoFacet = geoFacet;
     }
 
     public List<Multimedium> getMultimedia() {
