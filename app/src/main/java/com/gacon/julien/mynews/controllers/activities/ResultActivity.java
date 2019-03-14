b82c6d5e70304eac22478647f9f9f8d07146a086
Julien Gacon <gaconjulien@gmail.com>
2019-03-14 15:23:57 +0100

    Revert "Add ToolBar Arrow"
    This reverts commit 6a1fe0e

--- app/src/main/java/com/gacon/julien/mynews/controllers/activities/ResultActivity.java
+++ app/src/main/java/com/gacon/julien/mynews/controllers/activities/ResultActivity.java
@@ -1,14 +1,11 @@
 package com.gacon.julien.mynews.controllers.activities;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.gacon.julien.mynews.R;
 import com.gacon.julien.mynews.controllers.fragments.searchFragment.MainSearchFragment;
 import com.gacon.julien.mynews.controllers.fragments.searchFragment.SearchResultFragment;
-
-import java.util.Objects;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -22,7 +19,7 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        configureToolbar();
+
         this.getData();
         this.configureAndShowMainFragment();
     }
@@ -62,16 +59,5 @@ public class ResultActivity extends AppCompatActivity {
                     .add(R.id.frame_layout_result, searchResultFragment)
                     .commit();
         }
-    }
-
-    private void configureToolbar(){
-        //Get the toolbar view inside the activity layout
-        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        // Set the Toolbar
-        setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        // Enable the up button
-        assert ab != null;
-        Objects.requireNonNull(ab).setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/gacon/julien/mynews/controllers/activities/WebViewActivity.java
+++ app/src/main/java/com/gacon/julien/mynews/controllers/activities/WebViewActivity.java
@@ -1,6 +1,5 @@
 package com.gacon.julien.mynews.controllers.activities;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
@@ -8,8 +7,6 @@ import android.webkit.WebViewClient;
 
 import com.gacon.julien.mynews.R;
 import com.gacon.julien.mynews.controllers.fragments.articleFragment.BaseArticleFragment;
-
-import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,23 +22,11 @@ public class WebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
         ButterKnife.bind(this);
-        configureToolbar();
         // Configure webview
         mWebViewClient = new WebViewClient();
         mWebView.setWebViewClient(mWebViewClient);
         String url=getIntent().getStringExtra(BaseArticleFragment.BUNDLE_URL);
         mWebView.loadUrl(url);
-    }
-
-    private void configureToolbar(){
-        //Get the toolbar view inside the activity layout
-        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        // Set the Toolbar
-        setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        // Enable the up button
-        assert ab != null;
-        Objects.requireNonNull(ab).setDisplayHomeAsUpEnabled(true);
     }
 
 }
