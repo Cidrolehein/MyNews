78c67bfed9e685d153c78f3d69a94ea26f725925
Julien Gacon <gaconjulien@gmail.com>
2019-03-14 15:22:58 +0100

    Revert "Change a bug in MainActivity due to the revert commit"
    This reverts commit 9d424c5

--- app/src/main/java/com/gacon/julien/mynews/controllers/activities/MainActivity.java
+++ app/src/main/java/com/gacon/julien/mynews/controllers/activities/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import com.gacon.julien.mynews.R;
 import com.gacon.julien.mynews.views.adapters.viewPager.PageAdapter;
 
+@SuppressWarnings("SpellCheckingInspection")
 public class MainActivity extends AppCompatActivity {
 
     @Override
